81cea65b1cdf11f56bb039c0edce2e929f834c38
Arun Ghosh <arunghosh@gmail.com>
2017-03-09 09:42:18 +0530

    corrected build issue

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,28 @@ const path = require('path');
 
 console.log(process.env.NODE_ENV)
 
-let entry = './src/index.js'
-
-if (process.env.NODE_ENV === 'dev') {
-  entry = './example/index.js';
-}
-
-console.log(entry);
-
-module.exports = {
-  entry: entry,
-  output: {
+let 
+  entry = './src/index.js',  
+  output = {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: 'bundle.js'
-  },
+  };
+
+if (process.env.NODE_ENV === 'dev') {
+  entry = './example/index.js';
+  output = {
+    path: path.join(__dirname, 'example'),
+    publicPath: '/example/',
+    filename: 'bundle.js'
+  };
+}
+
+console.log(entry, output);
+
+module.exports = {
+  entry: entry,
+  output: output, 
   resolve: {
     extensions: ['.js', '.jsx']
   },
